c9ded0d9768b26f7dfeb3023bf00d8ed1bdd8801
KhallilB <khallil.bailey@gmail.com>
2020-02-06 17:23:42 -0800

    [add] gen html file

--- makesite.go
+++ makesite.go
@@ -1,51 +1,90 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
-type entry struct {
-	Name string
-	Done bool
+type data struct {
+	Text string
 }
 
-type ToDo struct {
-	User string
-	List []entry
+func getFileName() (string, error) {
+	var file string
+
+	flag.StringVar(&file, "file", "", "File Name")
+	flag.Parse()
+
+	return file, nil
 }
 
-func main() {
-	paths := []string{
-		"todo.tmpl",
-	}
-
-	todos := []entry{
-		entry{Name: "Task", Done: false},
-	}
-	user := ToDo{
-		User: "Khallil",
-		List: todos,
-	}
-
-	t := template.Must(template.New("todo.tmpl").ParseFiles(paths...))
-	err := t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func readFile(src string) string {
-	content, err := ioutil.ReadFile(src)
+func readFile(text string) (string, error) {
+	content, err := ioutil.ReadFile(text)
 
 	if err != nil {
 		panic(err)
 	}
 
-	return string(content)
+	return string(content), nil
 }
 
 func writeFile(dst string, data []byte) error {
 	return ioutil.WriteFile(dst, data, 0644)
+}
+
+func createTemplate(text string, writer io.Writer) (string, error) {
+	content := data{Text: text}
+
+	tmpl, err := template.ParseFiles("template.html")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var buffer bytes.Buffer
+
+	if err := tmpl.Execute(&buffer, content); err != nil {
+		return "", fmt.Errorf("error rendering data into template")
+	}
+
+	return buffer.String(), nil
+}
+
+func writeToTemplate(text string, writer io.Writer) error {
+	content := data{Text: text}
+
+	tmpl, err := template.ParseFiles("template.html")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tmpl.Execute(os.Stdout, content); err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func main() {
+	file, err := getFileName()
+
+	data, err := readFile(file)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err = writeToTemplate(data, w)
+
+		if err != nil {
+			log.Fatal("Could could not render data into html.")
+		}
+	})
+
+	http.ListenAndServe(":2525", nil)
 }
